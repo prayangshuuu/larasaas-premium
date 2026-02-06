95fb18978c215d81caac7e5df3788ae2f264285a
Prayangshu <prayangshubiswashritwick@gmail.com>
2026-02-06 15:09:22 +0600

    fix(2fa): resolve undefined slot error in recovery codes view

--- resources/views/profile/two-factor-recovery-codes.blade.php
+++ resources/views/profile/two-factor-recovery-codes.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
+<x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="max-w-3xl mx-auto">
@@ -54,4 +52,4 @@
             </div>
         </div>
     </div>
-@endsection
+</x-app-layout>
